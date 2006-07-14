58963cb67f5715b8a40c108746c7a4ec016de1ba
edy <edy@fa0f0c2e-3a18-0410-ba74-be68f72a8723>
2006-07-14 22:33:22 +0000

    die wichtigste klasse angelegt :)

--- trunk/s7ncms/lib/s7ncms.php
+++ trunk/s7ncms/lib/s7ncms.php
@@ -7,4 +7,84 @@
  * @version $Id$
  * @copyright Eduard Baun, 2006
  */
+
+class S7Ncms {
+    public $cfg;
+    public $db;
+    public $param;
+    
+    /**
+     * initializes some instances
+     *
+     */
+    public function __construct() {
+        /*
+         * TODO: Instanzen einbauen
+         * - Parameter vielleicht?! glaub nich... naja, we'll see :o
+         * - Datenbank
+         * - Konfiguration
+         * - Benutzerverwaltung (Zugriffsrechte)
+         */
+    }
+    
+    /**
+     * Prints out the generated output
+     *
+     */
+    public function __destruct() {
+        /*
+         * TODO: Hier kommt die Ausgabe rein
+         * $template = new Template('main');
+         * echo $template->parse(array(
+         *     'title' => $this->cfg->get('s7ncms','title'),
+         *     'content' => $this->output
+         * ));
+         */
+    }
+    
+    /**
+     * gets the module
+     *
+     * @return mixed Modulname or null if none given
+     */
+    public function getRequestedModule() {
+        if (    array_key_exists('module',$_GET)
+            AND preg_match("/^([a-zA-Z]+)$/",$_GET['module'],$match)) {
+    		return strtolower($match[1]);
+        }
+        return null;
+    }
+    
+    /**
+     * gets the class
+     *
+     * @return mixed null if none set; int if number (you can use it as id); string if class set
+     */
+    public function getRequestedClass() {
+    	if (    array_key_exists('class',$_GET)
+    	    AND ctype_digit($_GET['class'])) {
+			return $_GET['class'];
+		} elseif (array_key_exists('class',$_GET)
+			AND preg_match("/^([a-zA-Z]+)$/",$_GET['class'],$match)) {
+		    return strtolower($match[1]);
+		} else {
+		    return null;
+		}
+    }
+    
+    /**
+     * gets the event
+     *
+     * @return mixed null if no event set otherwise the requested event
+     */
+    public function getRequestedEvent() {
+    	if (    array_key_exists('event',$_GET)
+    		AND preg_match("/^([a-zA-Z]+)$/",$_GET['event'],$match)) {
+			return strtolower($match[1]);
+		} else {
+			return null;
+		}        
+    }
+    
+}
 ?>
