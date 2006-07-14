9671c742a63931373d0046bba592c950bd25fcb2
edy <edy@fa0f0c2e-3a18-0410-ba74-be68f72a8723>
2006-07-14 22:52:51 +0000

    Datenbankinterface erstellt. Vielleicht wird daraus ja auch eine abstrakte Klasse.

--- trunk/s7ncms/lib/interface/database.php
+++ trunk/s7ncms/lib/interface/database.php
@@ -8,4 +8,47 @@
  * @copyright Eduard Baun, 2006
  */
 
+/**
+ * Database Interface
+ */
+interface S7N_Interface_Database {
+    /**
+     * Send a query to database
+     *
+     */
+    public function query();
+    
+    /**
+     * Send multimpe query to database which are concatenated by a semicolon
+     *
+     */
+    public function multipleQuery();
+    
+    /**
+     * Affected Rows in the last query
+     *
+     */
+    public function affectedRows();
+    
+    /**
+     * Last inserted ID of the last query
+     *
+     */
+    public function lastInsertId();
+    
+    /**
+     * Escapes special characters in a string for use in a SQL statement
+     *
+     * @param string $string as a reference
+     */
+    public function escapeString(&$string);
+    
+    /**
+     * Fetch a result row as an associative array
+     *
+     * @param resource $result
+     */
+    public function fetchAssoc($result);
+}
+
 ?>
